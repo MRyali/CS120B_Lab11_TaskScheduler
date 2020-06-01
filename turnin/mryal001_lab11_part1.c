900f99de261f77674d7155758842eabfb802302e
Mayur Ryali <43820294+MRyali@users.noreply.github.com>
2020-06-01 15:01:12 -0700

    Completed part 1. May need to rework period values

--- turnin/mryal001_lab11_part1.c
+++ turnin/mryal001_lab11_part1.c
@@ -1,23 +1,138 @@
-/*	Author: lab
- *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
+/*	Author: Mayur Ryali
+ *  Partner(s) Name:
+ *	Lab Section: 21
+ *	Assignment: Lab #11  Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
+
+// Demo: 
+
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
+#include "keypad.h"
+#include "scheduler.h"
+#include "io.h"
+#include "timer.h"
 #endif
 
-int main(void) {
-    /* Insert DDR and PORT initializations */
+enum States {Start, runSM} state;
 
-    /* Insert your solution below */
-    while (1) {
+unsigned char keypadVal;
+unsigned char tempB;
 
+int tick(int state) {
+    keypadVal = GetKeypadKey();
+    switch(state) {
+        case Start:
+            state = runSM;
+            break;
+        case runSM:
+            switch(keypadVal) { //get keypad val then wait for next keypad val upon returning to the same state
+                case '\0':
+      			       tempB = 0x1F;
+                       break;
+      			case '1':
+      			       tempB = 0x01;
+                       break;
+      			case '2':
+      			       tempB = 0x02;
+      			       break;
+      			case '3':
+      			       tempB = 0x03;
+      		           break;
+      			case '4':
+      			       tempB = 0x04;
+      		           break;
+      			case '5':
+      			       tempB = 0x05;
+      		           break;
+      			case '6':
+      			       tempB = 0x06;
+      		           break;
+      			case '7':
+      			       tempB = 0x07;
+      		           break;
+      			case '8':
+      			       tempB = 0x08;
+      			       break;
+      			case '9':
+      			       tempB = 0x09;
+      			       break;
+      			case 'A':
+      			       tempB = 0x0A;
+      			       break;
+      			case 'B':
+      			       tempB = 0x0B;
+      			       break;
+      			case 'C':
+      			       tempB = 0x0C;
+      			       break;
+      			case 'D':
+      			       tempB = 0x0D;
+      			       break;
+      			case '*':
+      			       tempB = 0x0E;
+      			       break;
+      			case '0':
+      			       tempB = 0x00;
+      			       break;
+      			case '#':
+      			       tempB = 0x0F;
+      			       break;
+      			default:
+      			       tempB = 0x1B;
+      			       break;
+            }
+            state = runSM; //keep getting input from kepyad
+            break;
     }
-    return 1;
+    switch(state) {
+        case Start:
+            PORTB = 0x1F; //starting value
+            break;
+        case runSM:
+            PORTB = tempB;
+        default:
+            PORTB = tempB;
+            break;
+    }
+    return state;
+}
+
+int main(void) {
+    DDRC = 0x00; PORTC = 0xFF; //input
+    DDRB = 0xFF; PORTB = 0x00; //output
+    DDRD = 0xFF; PORTD = 0x00; //output;
+
+    // unsigned long int
+    static task task1;
+    task *tasks[] = { &task1};
+    const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
+
+	   // Task 1
+	task1.state = 0;//Task initial state.
+	task1.period = 2;//Task Period.
+	task1.elapsedTime = 2;//Task current elapsed time.
+    task1.TickFct = &tick;//Function pointer for the tick.
+
+    TimerSet(10);
+	TimerOn();
+
+    unsigned short i; // Scheduler for-loop iterator
+   	while(1) {
+   	    for ( i = 0; i < numTasks; i++ ) {
+   		       if ( tasks[i]->elapsedTime == tasks[i]->period ) {
+   			          tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
+   				      tasks[i]->elapsedTime = 0;
+   			}
+   			tasks[i]->elapsedTime += 1;
+   		}
+   		while(!TimerFlag);
+   		TimerFlag = 0;
+   	}
+    return 0;
 }
